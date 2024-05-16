da8b54de791bbf241ad1894031d33e7a8beaa625
danilo <danilo806@icloud.com>
2024-05-16 13:52:14 +0200

    feat: add logout func
    add logout function

--- src/fortune-wheel-booth-frontend/src/App.tsx
+++ src/fortune-wheel-booth-frontend/src/App.tsx
@@ -1,25 +1,24 @@
 import { Scanner } from '@yudiel/react-qr-scanner';
 import { useCallback, useEffect, useState } from 'react';
-import { canisterId, createActor } from 'declarations/fortune-wheel-booth-backend';
+import {
+  canisterId,
+  createActor,
+} from 'declarations/fortune-wheel-booth-backend';
 import { AuthClient } from '@dfinity/auth-client';
 import { HttpAgent } from '@dfinity/agent';
 import { _SERVICE } from 'declarations/fortune-wheel-booth-backend/fortune-wheel-booth-backend.did';
-import { type ActorSubclass, Identity } from "@dfinity/agent";
-import { Principal } from "@dfinity/principal";
+import { type ActorSubclass, Identity } from '@dfinity/agent';
+import { Principal } from '@dfinity/principal';
 
 export default function Home() {
-  const [showScanner, setShowScanner] = useState(false);
   const [adminActor, setAdminActor] = useState<ActorSubclass<_SERVICE>>();
   const [adminPrincipal, setAdminPrincipal] = useState<Principal>();
 
   const setupIcState = useCallback((identity: Identity) => {
     const agent = new HttpAgent({ identity });
-    const actor = createActor(
-      canisterId,
-      {
-        agent,
-      }
-    );
+    const actor = createActor(canisterId, {
+      agent,
+    });
     setAdminActor(actor);
 
     setAdminPrincipal(identity.getPrincipal());
@@ -36,9 +35,10 @@ export default function Home() {
     // start the login process and wait for it to finish
     await new Promise((resolve) => {
       authClient.login({
-        identityProvider: process.env.DFX_NETWORK === "ic"
-          ? "https://identity.ic0.app"
-          : `http://rdmx6-jaaaa-aaaaa-aaadq-cai.localhost:4943/`,
+        identityProvider:
+          process.env.DFX_NETWORK === 'ic'
+            ? 'https://identity.ic0.app'
+            : `http://rdmx6-jaaaa-aaaaa-aaadq-cai.localhost:4943/`,
         onSuccess: resolve,
       });
     });
@@ -59,6 +59,13 @@ export default function Home() {
       }
     })();
   }, [setupIcState, resetIcState]);
+
+  const logout = useCallback(async () => {
+    if (adminActor) {
+      await (await AuthClient.create()).logout();
+      resetIcState();
+    }
+  }, [adminActor, resetIcState]);
 
   if (!adminPrincipal) {
     return <div>Loading...</div>;
@@ -95,15 +102,27 @@ export default function Home() {
             onResult={(text, result) => console.log(text, result)}
             onError={(error) => console.log('Error', error?.message)}
           />
-          <p className='absolute bottom-[15%] left-0 right-0 m-auto text-center text-white text-base'>
-            Admin Principal:
-            <br />
-            <span>{adminPrincipal.toText()}</span>
-          </p>
+          <div className='w-full flex justify-center items-center'>
+            <button
+              className='bg-white rounded-xl shadow-sm w-24 p-1 px-0 mx-auto'
+              onClick={logout}
+            >
+              <p className='text-center text-base'>Logout</p>
+            </button>
+          </div>
+          <div className='absolute bottom-[15%] left-0 right-0 m-auto flex justify-center items-center flex-col gap-4'>
+            <p className='text-center text-white text-base px-20 gap-2 font-bold'>
+              Admin Principal:
+              <br />
+              <span className='font-normal text-xs'>
+                {adminPrincipal.toText()}
+              </span>
+            </p>
+          </div>
         </>
       )}
       <img
-        className='absolute bottom-1 left-0 right-0 m-auto h-20 z-20'
+        className='absolute bottom-0 left-0 right-0 m-auto h-20 z-20'
         src='/logo2.svg'
         alt='DFINITY logo'
       />
